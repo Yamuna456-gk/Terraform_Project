5b15549add30c90a139ae3d7a3f5a7e81fd06d9e
yamunagk <Yamuna456-gk>
2026-05-01 22:29:07 +1000

    updated the format

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
 resource "aws_instance" "example" {
-  ami = aws_secretsmanager_secret_version.ec2_ami.secret_string
+  ami           = aws_secretsmanager_secret_version.ec2_ami.secret_string
   instance_type = "t3.micro"
 }
